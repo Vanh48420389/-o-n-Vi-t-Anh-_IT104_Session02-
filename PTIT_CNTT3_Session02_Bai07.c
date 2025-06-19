16a607f79e2d27cfc7ef27110a563b383dd8ec1b
Vanh48420389 <sivanh740@gamil.com>
2025-06-19 21:34:48 +0700

    push code

--- PTIT_CNTT3_Session02_Bai07.c
+++ PTIT_CNTT3_Session02_Bai07.c
@@ -8,7 +8,8 @@ int main()
 {
     int n;
     int *arr;
-    int number;
+    int number ;
+    int found =0;
 
     printf("nhap so phan tu :");
     scanf("%d", &n);
@@ -22,8 +23,33 @@ int main()
     for (int i = 0; i < n; i++)
     {
         printf("nhap phan tu thu %d", i + 1);
-        scanf("%d", arr[i]);
+        scanf("%d", &arr[i]);
     }
+
+    printf("nhap 1 so nguyen bat ki :");
+    scanf("%d", &number);
+
+    for (int i = 0; i < n-1 && !found; i++)
+    {
+        for (int j = i +1; j < n; j++)
+        {
+            if (arr[i] + arr[j]== number)
+            {
+                printf("%d + %d = %d", arr[i], arr[j], number);
+                found = 1;
+                break;
+            }
+        }
+    }
+
+    if (!found)
+    {
+        printf("khong tim thay cap nao trong mang ");
+    }
+
+    free(arr);
+
+    return 0;
 
 
 
